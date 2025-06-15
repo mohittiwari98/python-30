45cfb7018ba7e661d7d1489eb4ef0414c934c0cc
Mohit Tiwari <mohittiwari17091709@gmail.com>
2025-06-15 16:40:28 +0530

    binary_search.py

---
+++ binary_search.py
@@ -0,0 +1,17 @@
+def binarySearch(array,x,low,high):
+    while low<high:
+        mid =low+(high-low)//2
+        if x==array[mid]:
+            return mid 
+        elif x> array[mid]:
+            low =mid+1
+        else:
+            high=mid-1
+        return -1
+array=[3,4,5,6,7,8]
+x=4
+result=binarySearch(array,x,0,len(array)-1)
+if result!=-1:
+    print("element is present at index"+str(result))
+else:
+    print("not found")
