df9c18f36832c12f3209ae02695e28ede7b5301b
Mohit Tiwari <mohittiwari17091709@gmail.com>
2025-06-15 17:27:44 +0530

    time_to_second.py

---
+++ time_to_second.py
@@ -0,0 +1,6 @@
+days = int(input("input days"))
+hours = int(input("enter hours"))
+minutes = int(input("enter minutes"))
+second = int(input("enter seconds"))
+time = days+hours+minutes+second
+print("the amount of seconds",time)
