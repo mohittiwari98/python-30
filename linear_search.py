e926a148d9168b3f1a287275557c5edf96b603ed
Mohit Tiwari <mohittiwari17091709@gmail.com>
2025-06-15 16:52:47 +0530

    linear_search.py

---
+++ linear_search.py
@@ -0,0 +1,14 @@
+def linearsearch(array,n,x):
+    for i in range(0,n):
+        if(array[i]==x):
+            return i 
+        return -1
+
+array =[1,2,5,4,7,5]
+x=1
+n=len(array)
+result = linearsearch(array,n,x)
+if(result == -1):
+    print("element not found")
+else:
+    print("element found at index: ",result)
