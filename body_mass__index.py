c6372f98ce249e51eeae1610d1a6441afbabb9d1
Mohit Tiwari <mohittiwari17091709@gmail.com>
2025-06-17 19:28:49 +0530

    body_mass__index.py

--- body_mass__index.py
+++ body_mass__index.py
@@ -1,3 +1,4 @@
+#code for calculating bmi of your body
 height = float(input("enter your height"))
 weight = float(input("enter your weight"))
 print("your body mass index is:",round(weight/(height*height),2))
