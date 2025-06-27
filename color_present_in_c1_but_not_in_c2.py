a2a2e4184d4d0bcc8a0e85e96d58d1ea95ae2d4e
Mohit Tiwari <mohittiwari17091709@gmail.com>
2025-06-27 12:38:19 +0530

    color_present_in_c1_but_not_in_c2.py

--- color_present_in_c1_but_not_in_c2.py
+++ color_present_in_c1_but_not_in_c2.py
@@ -1,4 +1,4 @@
-#find color present in
+#find color present in color1
 color_list_1 = set(["white","black","red"])
 color_list_2 = set(["white","black"])
 print(color_list_1.difference(color_list_2))
