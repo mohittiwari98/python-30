69d8e4ee695e00567714187c38921fd6ff217a6a
Mohit Tiwari <mohittiwari17091709@gmail.com>
2025-06-27 11:40:43 +0530

    binary_search.py

--- binary_search.py
+++ binary_search.py
@@ -1,6 +1,6 @@
 #code for binary search 
 def binarySearch(array,x,low,high):
-    #using while
+    #using while loop
     while low<high:
         mid =low+(high-low)//2
         if x==array[mid]:
