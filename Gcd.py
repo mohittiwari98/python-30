6854be632017cd6cc8589e1dc529b2d1ffd9997e
mohittiwari98 <mohittiwari17091709@gmail.com>
2025-08-10 19:52:47 +0530

    Update Gcd.py

--- Gcd.py
+++ Gcd.py
@@ -6,7 +6,7 @@ def gcd(x,y):
         return y 
         #used for loop
     for k in range(int(y/2),0,-1):
-        #
+        #u
         if x%k==0 and y%k==0:
             gcd =k
             break
@@ -14,6 +14,7 @@ def gcd(x,y):
      
 print(gcd(12,15))
 print(gcd(4,6))
+
 
 
 
